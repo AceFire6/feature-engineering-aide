e3f283630c353b5d3c1fd7d9db9bcb85090b09f3
Jethro Muller <git@jethromuller.co.za>
2021-04-19 22:23:11 +0200

    Redo data model to support ordinals & do proper encoding

--- experiment_config/experiment.py
+++ experiment_config/experiment.py
@@ -4,11 +4,9 @@ from pathlib import Path
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 import toml
 
 from experiment_config.settings import SUPPORTED_CLASSIFIERS, SUPPORTED_METRICS
-from experiment_config.utils import get_encoding_from_label
 
 
 class Experiment:
@@ -21,40 +19,71 @@ class Experiment:
         self.prediction_true_values = experiment_config['data']['true_values']
         self.prediction_false_values = experiment_config['data']['false_values']
 
-        self.boolean_features = experiment_config['data']['bool_type_features']
-        self.other_data_type_features = experiment_config['data']['other_type_features']
+        self.feature_map = experiment_config['data']['features']
+        self.boolean_features = self.feature_map.get('bool', [])
+        self.ordinal_features = self.feature_map.get('ordinal', {})
+        self.categorical_features = self.feature_map.get('categorical', [])
+
+        # We exclude booleans because we set those after the NA values are removed
+        self._categorical_columns = self.categorical_features + list(self.ordinal_features.keys())
+        dtype_map = {
+            'category': self._categorical_columns,
+        }
+        self.dtypes = {}
+        for dtype, feat_list in dtype_map.items():
+            for column in feat_list:
+                self.dtypes[column] = dtype
 
         self.prediction_data_columns = experiment_config['data']['selected_features']
         self.target_column = experiment_config['data']['target']
         self.split_column = experiment_config['data']['data_split_column']
+        self.holdout_split_condition = experiment_config['data']['holdout_split_condition']
 
         self.prediction_data: pd.DataFrame = pd.read_csv(
             filepath_or_buffer=self.prediction_data_file,
-            dtype=self.other_data_type_features,
+            dtype=self.dtypes,
             usecols=self.prediction_data_columns + [self.target_column, self.split_column],
             true_values=self.prediction_true_values,
             false_values=self.prediction_false_values,
             na_values=experiment_config['data']['na_values'],
         )
         self.prediction_data.dropna(inplace=True)
-        self.prediction_data = self.prediction_data.astype(self.boolean_features)
-
-        self.label_encoded_cols = defaultdict(LabelEncoder)
-
-        self.training_data = self.prediction_data.apply(
-            lambda x: self.label_encoded_cols[x.name].fit_transform(x)
+        self.prediction_data = self.prediction_data.astype(
+            {column: 'bool' for column in self.boolean_features}
         )
 
-        self.holdout_split_condition = get_encoding_from_label(
-            column=self.split_column,
-            label=experiment_config['data']['holdout_split_condition'],
-            encoders=self.label_encoded_cols,
-        )
-        holdout_index = self.training_data[self.split_column] == self.holdout_split_condition
+        # Do OrdinalEncoding
+        self.ordinal_encoded_cols = defaultdict()
+        for column, category_ordering in self.ordinal_features.items():
+            self.prediction_data[column].cat.set_categories(
+                category_ordering,
+                ordered=True,
+                inplace=True,
+            )
+            self.ordinal_encoded_cols[column] = self.prediction_data[column].cat.categories
+            self.prediction_data[column] = self.prediction_data[column].cat.codes
 
-        self.X = self.training_data[~holdout_index][self.prediction_data_columns]
-        self.y = self.training_data[~holdout_index][self.target_column]
-        self.groups = self.training_data[~holdout_index][self.split_column]
+        # Do OneHotEncoding
+        if self.categorical_features:
+            self.prediction_data = pd.get_dummies(
+                self.prediction_data,
+                columns=self.categorical_features,
+                prefix=self.categorical_features,
+            )
+
+            for feature in self.categorical_features:
+                encoded_features = [
+                    column for column in self.prediction_data.columns
+                    if feature in column
+                ]
+                self.prediction_data_columns.remove(feature)
+                self.prediction_data_columns.extend(encoded_features)
+
+        holdout_index = self.prediction_data[self.split_column] == self.holdout_split_condition
+
+        self.X = self.prediction_data[~holdout_index][self.prediction_data_columns]
+        self.y = self.prediction_data[~holdout_index][self.target_column]
+        self.groups = self.prediction_data[~holdout_index][self.split_column]
 
         self.name = experiment_config['experiment']
         self.metric_results = {}
@@ -71,7 +100,7 @@ class Experiment:
         self.file_path = file_path
 
     def training_set_sample_size(self):
-        return self.training_data[self.target_column].size
+        return self.prediction_data[self.target_column].size
 
     def get_x_train_test_split(self, train_indices, test_indices):
         return self.X.iloc[train_indices], self.X.iloc[test_indices]
