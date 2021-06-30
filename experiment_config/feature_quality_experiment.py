a8d8c1701c1b986cc1692c472a9b230d2d4cc0fb
Jethro Muller <git@jethromuller.co.za>
2021-07-01 00:53:18 +0200

    Add debug info print statement

--- experiment_config/feature_quality_experiment.py
+++ experiment_config/feature_quality_experiment.py
@@ -1,10 +1,21 @@
 import sys
 
 from experiment_config.experiment import Experiment, parse_experiment_paths
+from experiment_config.settings import MEMORY_LIMIT, N_JOBS, TASK_TIME, TIME_PER_RUN, TOTAL_MEMORY_LIMIT
 
 
 def run_experiments(experiments: list[Experiment]) -> None:
-    pass
+    total_experiments = len(experiments)
+    print(
+        f'Running {total_experiments} experiment(s)!',
+        'Config:',
+        f'\tn_jobs = {N_JOBS}',
+        f'\ttotal_time = {TASK_TIME}',
+        f'\ttime_per_run = {TIME_PER_RUN}',
+        f'\ttotal_memory_limit = {TOTAL_MEMORY_LIMIT}',
+        f'\tmemory_limit_per_run = {MEMORY_LIMIT}',
+        sep='\n',
+    )
 
 
 if __name__ == '__main__':
