a7588c3ff0260a34014a18336ea5a6e5e324e0d5
Jethro Muller <git@jethromuller.co.za>
2021-07-11 17:51:42 +0200

    Add experiment_template.py file

---
+++ feature_engineer/experiment_template.py
@@ -0,0 +1,20 @@
+from logging import Logger
+import sys
+from typing import Optional
+
+from feature_engineer.experiment_config.experiment import Experiment
+from feature_engineer.experiment_runner.runner import ExperimentResult, ExperimentRunner
+
+
+class ExperimentName(ExperimentRunner):
+    def experiment(self, experiment: Experiment, logger: Optional[Logger] = None) -> ExperimentResult:
+        pass
+
+
+if __name__ == '__main__':
+    experiment_input_paths = sys.argv[1:]
+    if not experiment_input_paths:
+        print('Please pass in experiment files as arguments to this script')
+
+    experiment_results = ExperimentName(*experiment_input_paths, use_random_seeds=True)
+    print(f'Experiments finished - {experiment_results}')
